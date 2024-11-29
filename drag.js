0ef3779ef4a4d14a8c6f92148797bfa11b6200f2
bijaykhanal08 <bijaykhanal08@gmail.com>
2024-11-29 20:56:32 +0545

    Add files via upload

---
+++ drag.js
@@ -0,0 +1,92 @@
+    function moveLogo() {
+      const topValue = document.getElementById('logo-top').value;
+    document.getElementById('dlt').innerHTML = topValue/4+"%";
+
+      const leftValue = document.getElementById('logo-left').value;
+    document.getElementById('dll').innerHTML = leftValue/4+"%";
+
+      document.getElementById('logo').style.top = topValue + 'px';
+      document.getElementById('logo').style.left = leftValue + 'px';
+    }
+
+    function moveName() {
+      const topValue = document.getElementById('name-top').value;
+       document.getElementById('dnt').innerHTML = topValue/4+"%";
+
+      const leftValue = document.getElementById('name-left').value;
+    document.getElementById('dnl').innerHTML = leftValue/4+"%";
+
+      document.getElementById('npa').style.top = topValue + 'px';
+      document.getElementById('npa').style.left = leftValue + 'px';
+    }
+
+    function moveTt() {
+      const topValue = document.getElementById('moveTitleT').value;
+      document.getElementById('dtt').innerHTML = topValue / 4 + "%";
+
+      const leftValue = document.getElementById('moveTitleL').value;
+      document.getElementById('dtl').innerHTML = leftValue / 4 + "%";
+
+      document.getElementById('statuS').style.top = topValue + 'px';
+      document.getElementById('statuS').style.left = leftValue + 'px';
+    }
+
+    function moveP() {
+      const topValue = document.getElementById('movePhotoT').value;
+      document.getElementById('dpt').innerHTML = topValue / 4 + "%";
+
+      const leftValue = document.getElementById('movePhotoL').value;
+      document.getElementById('dpl').innerHTML = leftValue / 4 + "%";
+
+      document.getElementById('pp').style.top = topValue + 'px';
+      document.getElementById('pp').style.left = leftValue + 'px';
+    }
+
+    function moveN() {
+      const topValue = document.getElementById('moveNameT').value;
+      document.getElementById('dNt').innerHTML = topValue / 4 + "%";
+
+      const leftValue = document.getElementById('moveNameL').value;
+      document.getElementById('dNl').innerHTML = leftValue / 4 + "%";
+
+      document.getElementById('bName').style.top = topValue + 'px';
+      document.getElementById('bName').style.left = leftValue + 'px';
+    }
+
+
+    function moveCP() {
+      const topValue = document.getElementById('moveCpT').value;
+      document.getElementById('dCpt').innerHTML = topValue / 4 + "%";
+
+      const leftValue = document.getElementById('moveCpL').value;
+      document.getElementById('dCpl').innerHTML = leftValue / 4 + "%";
+
+      document.getElementById('clsp').style.top = topValue + 'px';
+      document.getElementById('clsp').style.left = leftValue + 'px';
+    }
+
+    function moveMs() {
+      const topValue = document.getElementById('moveMsT').value;
+      document.getElementById('dMst').innerHTML = topValue / 4 + "%";
+
+      const leftValue = document.getElementById('moveMsL').value;
+      document.getElementById('dMsl').innerHTML = leftValue / 4 + "%";
+
+      document.getElementById('wishess').style.top = topValue + 'px';
+      document.getElementById('wishess').style.left = leftValue + 'px';
+    }
+
+    function moveD() {
+      const topValue = document.getElementById('moveDT').value;
+      document.getElementById('dDt').innerHTML = topValue / 4 + "%";
+
+      const leftValue = document.getElementById('moveDL').value;
+      document.getElementById('dDl').innerHTML = leftValue / 4 + "%";
+
+      document.getElementById('daTe').style.top = topValue + 'px';
+      document.getElementById('daTe').style.left = leftValue + 'px';
+    }
+
+
+
+
