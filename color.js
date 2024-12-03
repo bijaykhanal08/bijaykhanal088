ec0efdc52b06dbc92149af38242e5e84bb2cf07a
bijaykhanal08 <bijaykhanal08@gmail.com>
2024-12-03 19:12:39 +0545

    Update color.js

--- color.js
+++ color.js
@@ -1,36 +1,83 @@
 const colovalue = document.querySelector('#color');
+
 colovalue.addEventListener('input', function() {
-  document.querySelector('#npa').style.color = colovalue.value;
+
+document.querySelector('#npa').style.color = colovalue.value;
+
 });
+
 const colovalueA = document.querySelector('#colorA');
+
 colovalueA.addEventListener('input', function() {
-  document.querySelector('#logo').style.borderColor = colovalueA.value;
+
+document.querySelector('#logo').style.borderColor = colovalueA.value;
+
 });
+
 const colovalueB = document.querySelector('#colorB');
+
 colovalueB.addEventListener('input', function() {
-  document.querySelector('#statuS').style.color = colovalueB.value;
+
+document.querySelector('#statuS').style.color = colovalueB.value;
+
 });
+
+
 
 const colovalueC = document.querySelector('#colorC');
+
 colovalueC.addEventListener('input', function() {
-  document.querySelector('#pp').style.borderColor = colovalueC.value;
+
+document.querySelector('#pp').style.borderColor = colovalueC.value;
+
 });
+
+
+
+const colovalueZ = document.querySelector('#colorZ');
+
+colovalueZ.addEventListener('input', function() {
+
+  document.querySelector('#Smata').style.borderColor = colovalueZ.value;
+
+});
+
+
+
 const colovalueD = document.querySelector('#colorD');
+
 colovalueD.addEventListener('input', function() {
-  document.querySelector('#bName').style.color = colovalueD.value;
+
+document.querySelector('#bName').style.color = colovalueD.value;
+
 });
+
+
 
 const colovalueE = document.querySelector('#colorE');
+
 colovalueE.addEventListener('input', function() {
-  document.querySelector('#clsp').style.color = colovalueE.value;
+
+document.querySelector('#clsp').style.color = colovalueE.value;
+
 });
+
+
 
 const colovalueF = document.querySelector('#colorF');
+
 colovalueF.addEventListener('input', function() {
-  document.querySelector('#daTe').style.color = colovalueF.value;
+
+document.querySelector('#daTe').style.color = colovalueF.value;
+
 });
 
+
+
 const colovalueI = document.querySelector('#colorI');
+
 colovalueI.addEventListener('input', function() {
-  document.querySelector('#wishess').style.color = colovalueI.value;
+
+document.querySelector('#wishess').style.color = colovalueI.value;
+
 });
